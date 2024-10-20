f45136539f1dcde14f574af5712a9109fc415835
Jan Halwax <if23b011@technikum-wien.at>
2024-10-20 17:54:39 +0200

    Database connected

---
+++ src/main/java/at/technikum/mtcg/DatabaseConnector.java
@@ -0,0 +1,39 @@
+package at.technikum.mtcg;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnector {
+    public static Connection connect() throws SQLException {
+        Properties props = new Properties();
+
+        try (InputStream input = DatabaseConnector.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Konfigurationsdatei 'config.properties' wurde nicht gefunden");
+            }
+            props.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Fehler beim Laden der Konfigurationsdatei", e);
+        }
+
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public static void main(String[] args) {
+        try (Connection connection = DatabaseConnector.connect()) {
+            if (connection != null) {
+                System.out.println("Verbindung zur Datenbank erfolgreich hergestellt!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Fehler bei der Verbindung zur Datenbank: " + e.getMessage());
+        }
+    }
+}
